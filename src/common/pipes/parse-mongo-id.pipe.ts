eccbdf58d44ab37a56394abd86fdef44fd85e4a1
Dario <darioe.barboza@gmail.com>
2022-09-11 06:10:39 -0300

    pokedex 02-nest-js

---
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -0,0 +1,22 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  BadRequestException
+} from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+
+  transform(value: string, metadata: ArgumentMetadata) {
+    /* Aclarar que los pipes transformar la información */
+    
+    if (!isValidObjectId(value))
+      throw new BadRequestException(`${value} is not a valid MongoID`);
+
+    
+    return value;
+  }
+
+}
---
+++ src/config/env.config.ts
@@ -0,0 +1,9 @@
+
+// Node environment siempre está
+
+export const EnvConfiguration = () => ({
+  environment: process.env.NODE_ENV || 'dev',
+  mongodb: process.env.MONGODB_URL,
+  port: process.env.PORT || 3002,
+  defaultLimit: +process.env.DEFAULT_LIMIT || 5,
+})
